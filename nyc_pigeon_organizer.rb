1b2179d620964467952653aad82466fde9fd2e93
Liam Healy <lhealy0711@gmail.com>
2019-09-14 17:45:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
   data[:color].each do |color, pigeon|
     pigeon.each do |pigeon_name|
       pigeons.each_key do |key|
-        if color.include?(key)
+        if key === color
           pigeons[key][:color].push(color.to_s)
         end
       end
