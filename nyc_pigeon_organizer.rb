962066962af31d0b92e3c50078e0262c81b8bbf2
Liam Healy <lhealy0711@gmail.com>
2019-09-14 17:39:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeons = {}
+  data.each do |attribute, category|
+    category.each do |value, list|
+      list.each do |pigeon_name|
+        pigeons[pigeon_name] = {:color => [], :gender => [], :lives => []}
+      end
+    end
+  end
+  pigeons.each_key do |key|
+    data[:color].each do |color, pigeon|
+      pigeon.each do |pigeon_name|
+        if pigeons[key] === color
+          pigeons[:color].push(color)
+        end
+      end
+    end
+  end
+  pigeons
 end
